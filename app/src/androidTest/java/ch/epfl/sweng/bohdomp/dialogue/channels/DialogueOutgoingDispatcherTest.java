5e9c5a7da031e391aa2cd0ae7c9c1deda1ee8e7b
Dennis van der Bij <dennis.vanderbij@epfl.ch>
2014-11-12 08:52:54 +0100

    Add null args tests

---
+++ app/src/androidTest/java/ch/epfl/sweng/bohdomp/dialogue/channels/DialogueOutgoingDispatcherTest.java
@@ -0,0 +1,35 @@
+package ch.epfl.sweng.bohdomp.dialogue.channels;
+
+import android.test.AndroidTestCase;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import ch.epfl.sweng.bohdomp.dialogue.conversation.contact.Contact;
+import ch.epfl.sweng.bohdomp.dialogue.conversation.contact.ContactFactory;
+import ch.epfl.sweng.bohdomp.dialogue.exceptions.NullArgumentException;
+import ch.epfl.sweng.bohdomp.dialogue.messaging.DialogueMessage;
+import ch.epfl.sweng.bohdomp.dialogue.messaging.DialogueTextMessage;
+
+public class DialogueOutgoingDispatcherTest extends AndroidTestCase {
+    public void testNullArgsSendMessage() throws Exception {
+        ContactFactory contactFactory = new ContactFactory(getContext());
+        Contact contact = contactFactory.contactFromNumber("123");
+
+        DialogueMessage message = new DialogueTextMessage(contact, "Hello", DialogueMessage.MessageStatus.OUTGOING);
+
+        try {
+            DialogueOutgoingDispatcher.sendMessage(null, message);
+            Assert.fail("Should fail with null context parameter.");
+        } catch (NullArgumentException e) {
+            // Success
+        }
+
+        try {
+            DialogueIncomingDispatcher.receiveMessage(getContext(), null);
+            Assert.fail("Should fail with null message parameter.");
+        } catch (NullArgumentException e) {
+            // Success
+        }
+    }
+}
